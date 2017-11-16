f9f9c8bdb5c1c7cde0b86f1c06530c2a00d79329
VILLE KOKKARINEN <kokkarinen.ville@gmail.com>
2017-11-16 13:14:05 +0200

    New stuff

--- Screenshot-Plugin/ISnapShotService.cs
+++ Screenshot-Plugin/ISnapShotService.cs
@@ -3,10 +3,20 @@ using Android.Graphics;
 
 namespace Screenshot_Plugin
 {
+    /// <summary>
+    /// Interface for taking a snapshot of the current view
+    /// </summary>
     public interface ISnapShotService
     {
-        void TakeScreenShot(View view = null);
-     
-        
+        /// <summary>
+        ///  Method to take a screenshot.              
+        ///  <para>View = View of which the screenshot will be taken. Null defaults to the root of current xamarin.forms </para>
+        ///  <para>   Path = Path on the device where to save. Null defaults to root/pictures </para>
+        ///  <para>Imagename = Name for Screenshot file + extension ".png" / ".jpeg" / ".jpg" </para>
+        /// </summary>
+        /// <param name="view"></param>
+        /// <param name="path"></param>
+        /// <param name="Imagename"></param>
+        void TakeSnapShot(View view = null, string path = null, string Imagename = null);        
     }
 }
--- Screenshot-Plugin/Screenshot.cs
+++ Screenshot-Plugin/Screenshot.cs
@@ -1,10 +1,9 @@
-﻿/// <summary>
-/// Open Source Screenshot Plugin for Android devices
-/// 
-/// 
-/// Ville Kokkarinen | @ | kokkarinen.ville@gmail.com
-/// 
-/// </summary>
+﻿// ---
+// Open Source Screenshot Plugin for Xamarin.forms 
+// 
+// Ville Kokkarinen | @ | kokkarinen.ville@gmail.com
+// 
+// ---
 
 using Android.App;
 using Android.Views;
@@ -13,13 +12,18 @@ using Android.Graphics;
 [assembly: Xamarin.Forms.Dependency(typeof(Screenshot_Plugin.SnapshotService))]
 namespace Screenshot_Plugin
 {
-
-    //surprise
     /// <summary>
-    /// Initializes a new instance of the <see cref="Screenshot"/> class.
+    /// Initializes a new instance of the <see cref="SnapshotService"/> class.
     /// </summary>
     public class SnapshotService : ISnapShotService
     {
+        /// <summary>
+        /// Generate this service and call it to take a snapshot
+        /// <para> Or call directly with: </para>
+        /// DependencyService.Get&lt;ISnapShotService>().TakeSnapShot()
+        /// </summary>
+        public SnapshotService() { }
+
 
         /// <summary>
         ///  Method to take a screenshot,
@@ -28,28 +32,28 @@ namespace Screenshot_Plugin
         ///  <para>Imagename = Name of the image + extension ".png" </para>
         /// </summary>
         /// <param name="view"></param>
-        /// <param name="Path"></param>
+        /// <param name="path"></param>
         /// <param name="Imagename"></param>
-        public void TakeScreenShot(View view = null)
+        public void TakeSnapShot(View view = null, string path=null, string Imagename=null)
         {
-            var screenshotPath =
-            Android.OS.Environment.GetExternalStoragePublicDirectory("Pictures").AbsolutePath +
-            Java.IO.File.Separator +
-            "screenshot.png";
+            if(view == null)
+                view = ((Activity)Xamarin.Forms.Forms.Context).Window.DecorView.RootView;
 
-            var rootView = ((Activity)Xamarin.Forms.Forms.Context).Window.DecorView.RootView;
+            if (path == null)
+            {
+                path = Android.OS.Environment.GetExternalStoragePublicDirectory("Pictures").AbsolutePath +
+                Java.IO.File.Separator + Imagename;                
+            }
 
             using (var screenshot = Bitmap.CreateBitmap(
-                    rootView.Width,
-                    rootView.Height,
+                    view.Width,
+                    view.Height,
                     Bitmap.Config.Argb8888))
             {
                 var canvas = new Canvas(screenshot);
-                rootView.Draw(canvas);
+                view.Draw(canvas);
 
-                using (var screenshotOutputStream = new System.IO.FileStream(
-                            screenshotPath,
-                            System.IO.FileMode.Create))
+                using (var screenshotOutputStream = new System.IO.FileStream(path, System.IO.FileMode.Create))
                 {
                     screenshot.Compress(Bitmap.CompressFormat.Png, 90, screenshotOutputStream);
                     screenshotOutputStream.Flush();
